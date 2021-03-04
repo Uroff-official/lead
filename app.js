06fb9d1159b71c80644a477607cd3449994d4c5a
javierUDP <javier.perezs@mail.udp.cl>
2021-03-04 13:09:20 -0300

    a
    a

--- app.js
+++ app.js
@@ -1,5 +1,43 @@
 var formApp = angular.module('formApp', ['ngAnimate', 'ui.router']);
 
+
+var cities = [
+              {
+                  city : 'India',
+                  desc : 'This is the best country in the world!',
+                  lat : 23.200000,
+                  long : 79.225487
+              },
+              {
+                  city : 'New Delhi',
+                  desc : 'The Heart of India!',
+                  lat : 28.500000,
+                  long : 77.250000
+              },
+              {
+                  city : 'Mumbai',
+                  desc : 'Bollywood city!',
+                  lat : 19.000000,
+                  long : 72.90000
+              },
+              {
+                  city : 'Kolkata',
+                  desc : 'Howrah Bridge!',
+                  lat : 22.500000,
+                  long : 88.400000
+              },
+              {
+                  city : 'Chennai  ',
+                  desc : 'Kathipara Bridge!',
+                  lat : 13.000000,
+                  long : 80.250000
+              }
+          ];
+
+
+
+
+
 formApp.config(function ($stateProvider, $urlRouterProvider) {
 
     $stateProvider
@@ -158,21 +196,45 @@ formApp.controller('formController', function ($scope, $http, $state) {
 
 });
 
-formApp.controller('mapCtrl', ['$scope','$rootScope', '$state', function ($scope, $rootScope, $state){
+formApp.controller('MapCtrl', function ($scope) {
 
-  $scope.myMarkers = [
-      {
-         "latitude":33.22,
-         "longitude":35.33
-      }
-  ];
+    var mapOptions = {
+        zoom: 4,
+        center: new google.maps.LatLng(25,80),
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    }
 
-  $scope.center = {
-      latitude: 33.895497,
-      longitude: 35.480347,
-  };
+    $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-  $scope.zoom = 13;
-  $scope.markers = $scope.myMarkers;
-  $scope.fit = true;
-}]);
+    $scope.markers = [];
+
+    var infoWindow = new google.maps.InfoWindow();
+
+    var createMarker = function (info){
+
+        var marker = new google.maps.Marker({
+            map: $scope.map,
+            position: new google.maps.LatLng(info.lat, info.long),
+            title: info.city
+        });
+        marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
+
+        google.maps.event.addListener(marker, 'click', function(){
+            infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+            infoWindow.open($scope.map, marker);
+        });
+
+        $scope.markers.push(marker);
+
+    }
+
+    for (i = 0; i < cities.length; i++){
+        createMarker(cities[i]);
+    }
+
+    $scope.openInfoWindow = function(e, selectedMarker){
+        e.preventDefault();
+        google.maps.event.trigger(selectedMarker, 'click');
+    }
+
+});
