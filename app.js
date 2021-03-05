5e7393f9fde10eb0977365cf74e7f451a2159ff6
javierUDP <javier.perezs@mail.udp.cl>
2021-03-05 12:31:37 -0300

    a
    a

--- app.js
+++ app.js
@@ -1,43 +1,6 @@
 var formApp = angular.module('formApp', ['ngAnimate', 'ui.router','gm']);
 
 
-var cities = [
-              {
-                  city : 'India',
-                  desc : 'This is the best country in the world!',
-                  lat : 23.200000,
-                  long : 79.225487
-              },
-              {
-                  city : 'New Delhi',
-                  desc : 'The Heart of India!',
-                  lat : 28.500000,
-                  long : 77.250000
-              },
-              {
-                  city : 'Mumbai',
-                  desc : 'Bollywood city!',
-                  lat : 19.000000,
-                  long : 72.90000
-              },
-              {
-                  city : 'Kolkata',
-                  desc : 'Howrah Bridge!',
-                  lat : 22.500000,
-                  long : 88.400000
-              },
-              {
-                  city : 'Chennai  ',
-                  desc : 'Kathipara Bridge!',
-                  lat : 13.000000,
-                  long : 80.250000
-              }
-          ];
-
-
-
-
-
 formApp.config(function ($stateProvider, $urlRouterProvider) {
 
     $stateProvider
